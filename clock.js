f4462c21ff2cc8b5f7f9d5ffd8edd249cd0ad781
Romil Rõbtšenkov <romilrobtsenkov@gmail.com>
2018-02-09 11:49:58 +0200

    Added sample background color change

--- clock.js
+++ clock.js
@@ -1,5 +1,6 @@
 // Avalikud muutujad
 let clockContainer // = null
+let changeColorButton 
 
 
 window.onload = function () {
@@ -9,14 +10,34 @@ window.onload = function () {
 function init () {
   clockContainer = document.querySelector('#clock')
   console.log(clockContainer)
+  changeColorButton = document.querySelector('#change-color')
 
   startClock()
+
+  changeColorButton.addEventListener('click', changeBackgroundColor)
+  window.addEventListener('mousemove', changeBackgroundColor)
+}
+
+function changeBackgroundColor () {
+  console.log('muudan värvi')
+
+  const r = Math.round(Math.random() * 255)
+  const g = Math.round(Math.random() * 255)
+  const b = Math.round(Math.random() * 255)
+
+  clockContainer.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')'
 }
 
 function startClock () {
-  window.setInterval(function () {
-    const date = new Date()
+  updateClock()
 
-    clockContainer.innerHTML = date
+  window.setInterval(function () {
+   updateClock()
   }, 1000)
+}
+
+function updateClock () {
+  const date = new Date()
+
+  clockContainer.innerHTML = date
 }
